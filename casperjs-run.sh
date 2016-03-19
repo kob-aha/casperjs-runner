1f3fe09ca4bf25a9e07cbacc446ed953e65b7f7c
Koby Aharon <kobyahron@gmail.com>
2016-03-19 10:49:29 +0200

    Update run script to support running script from a different folder

--- casperjs-run.sh
+++ casperjs-run.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+function getPath {
+    local __resultvar="$2"
+    local path="$( cd "$( dirname "$1" )" && pwd )"
+    
+    if [[ "$__resultvar" ]]; then
+        eval $__resultvar="'$path'"
+    else
+        echo "$path"
+    fi
+}
+
+getPath "${BASH_SOURCE[0]}" DIR
+
+# In case we got a script name to run we'll extract it's path
+if [[ "$1" ]]; then
+    getPath "$1" SCRIPT_PATH
+    SCRIPT_NAME="$(basename $1)"
+    SCRIPT="/scripts/${SCRIPT_NAME}"
+else
+    SCRIPT_PATH=${DIR}
+    SCRIPT=""
+fi
 
 IMAGE_NAME=casperjs
 
-docker run -v $DIR:/home/casperjs --rm --name casperjs ${IMAGE_NAME} "$@" 
+docker run -v $DIR:/home/casperjs -v $SCRIPT_PATH:/scripts --rm --name casperjs ${IMAGE_NAME} ${SCRIPT} 
